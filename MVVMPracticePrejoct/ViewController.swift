c01d8731df30f8946a09e756f67f7cd05a277d90
ashitosh39 <151606213+ashitosh39@users.noreply.github.com>
2025-01-22 17:46:25 +0530

    Initial Commit

---
+++ MVVMPracticePrejoct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPracticePrejoct
+//
+//  Created by Digitalflake on 22/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
